22dbbde6129e2da5e4164f0e81f2bd49f26ad04f
Terence Deng <dengtooling@gmail.com>
2013-12-27 23:42:38 +0800

    add option 'Enable Extension'

--- settings.js
+++ settings.js
@@ -7,14 +7,18 @@ $(document).ready(function() {
 		settings.fileExtension = $("#fileExtension").val();
 		settings.aria2RPCUri = $("#aria2RPCUri").val();
 		settings.notificationEnabled = $("#notificationEnabled").prop("checked");
+		settings.extensionEnabled = $("#extensionEnabled").prop("checked");
 		updateSettings(settings);
 	});
 
 	function load(s){
 		$('#aria2RPCUri').val(s.aria2RPCUri);
 		$('#fileExtension').val(s.fileExtension);
-		if(s.notificationEnabled == true) {
+		if(s.notificationEnabled) {
 			$('#notificationEnabled').attr("checked", s.notificationEnabled);
+		}
+		if(s.extensionEnabled) {
+			$('#extensionEnabled').attr("checked", s.extensionEnabled);
 		}
 	}
 	function getSettings(){
